84a1dae45750ba79147ebbb49d0e620dfe289d61
vagrant <vagrant@blogger>
2017-10-19 16:43:18 +0000

    Here we seting email sending when registring new user to our app

---
+++ app/Mail/Welcome.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\User;
+
+class Welcome extends Mailable
+{
+    use Queueable, SerializesModels;
+
+
+    public $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user) // Here we type hint or dinamicly bind User function instace to say that argument must be instance of user class
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.hello'); // Here we place email which we want to create - this is path to over views
+    }
+}
